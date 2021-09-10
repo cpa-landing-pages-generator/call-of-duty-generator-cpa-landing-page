20551485ffd4645d8c38a848ae96ee0bd63801b5
cpa-landing-pages-generator <90444422+cpa-landing-pages-generator@users.noreply.github.com>
2021-09-10 10:14:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,143 @@
+<!-- 
+
+//
+//
+//Landing Page Shared at CPALanding.com
+//https://www.cpalanding.com
+//
+//
+
+ -->
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+		<title>Call of Duty: Modern Warfare Online Generator</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="description" content="Call of Duty: Modern Warfare Online Generator" />    
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="icon" type="image/ico" href="img/favicon.ico" />
+		<!-- Open Graph Meta Tags-->
+		<meta property="og:title" content="Call of Duty: Modern Warfare Online Generator" /> <!-- Title which is displayed when your site is shared on social networks -->
+		<meta property="og:description" content="Call of Duty: Modern Warfare Online Generator" /> <!-- Website description which is displayed when your site is shared on social networks -->
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://www.mywebsiteurl.com/" /> <!-- Your Website URL -->
+		<meta property="og:image" content="http://www.mywebsiteurl.com/social-share-image.jpg" />	<!-- Absolute Path to the Image which will display, when your website is shared on social networks -->
+		<!-- Twitter Meta -->
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@tweetname" />
+		<meta name="twitter:title" content="Tweet Title" />
+		<meta name="twitter:description" content="Tweet Content" />
+		<meta name="twitter:image" content="http://www.mywebsiteurl.com/twitter-share-image.jpg" />
+		<!-- Icons -->
+		<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&display=swap" rel="stylesheet">
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" />  
+		<link href="css/animate.css" rel="stylesheet" />
+		<link href="css/magnific-popup.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+	</head>
+	<body>		
+		<div class="main-wrapper">		
+			
+				<header>
+					<div class="main-bg"></div>
+					<div class="branding-wrapper">						
+						<img src="img/cod.svg" class="img-fluid logo-img-1" alt="Call of Duty:" />
+						<img src="img/mw.svg" class="img-fluid logo-img-2" alt="Modern Warfare" />
+					</div>
+					<div class="char-wrapper"><div class="char"></div></div>
+				</header>
+				<div class="main-content">
+					<div class="container">
+						<div class="generator-wrapper">
+							<div class="generator-header-wrapper">
+								<div class="generator-header">
+									<h1 class="animated bounceIn animation-delay-200"><span class="h-bottom">Online Generator</span></h1>
+									<div class="generator-status">
+										<div class="generator-status-content">
+											<div class="status-item-wrapper animated bounceIn animation-delay-400">
+												<span class="status-label">Status:</span>
+												<span id="status" class="status-dynamic">Online</span>
+											</div>
+											<div class="status-item-wrapper status-item-wrapper-2 animated bounceIn animation-delay-600">
+												<span class="status-label">Last Update:</span>
+												<span id="date" class="status-dynamic"></span>
+											</div>
+											<div class="status-item-wrapper animated bounceIn animation-delay-800">
+												<span class="status-label">Online Users:</span>
+												<span id="online-count" class="status-dynamic"></span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="generator-content">								
+								<div class="form-wrapper">
+									<div class="label animated bounceIn animation-delay-400">Username and Device</div>
+									<div class="animated bounceIn animation-delay-600">
+										<div class="input-wrapper username-wrapper">
+											<img src="img/input-icon.png" class="input-icon" />
+											<input type="text" id="player-username" class="s-input" placeholder="Username..." />
+										</div>
+									</div>
+									<div class="animated bounceIn animation-delay-800">
+										<div class="platform-select-wrapper">
+											<div class="row small-margin">
+												<div class="col small-padding">
+													<div class="platform-item platform-item-1">
+														<i class="fab fa-windows"></i>
+														<span>Windows</span>
+													</div>
+												</div>
+												<div class="col small-padding">
+													<div class="platform-item platform-item-2">
+														<i class="fab fa-xbox"></i>
+														<span>Xbox</span>
+													</div>
+												</div>
+												<div class="col small-padding">
+													<div class="platform-item platform-item-3">
+														<i class="fab fa-playstation"></i>
+														<span>Playstation</span>
+													</div>
+												</div>
+												<div class="col small-padding">
+													<div class="platform-item platform-item-4">
+														<i class="fab fa-android"></i>
+														<span>Android</span>
+													</div>
+												</div>
+												<div class="col small-padding">
+													<div class="platform-item platform-item-5">
+														<i class="fab fa-apple"></i>
+														<span>iOS</span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="button-wrapper animated bounceIn animation-delay-1000">
+										<div id="proc-btn-1" class="cod-btn">
+											<span>Connect</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>		
+		</div>
+		<div class="modal-outer"></div>
+	</body>
+	<!-- JS -->	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>	
+	<script type="text/javascript" src="js/ion.sound.min.js"></script>	
+    <script type="text/javascript" src="js/jquery.countTo.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</html>
